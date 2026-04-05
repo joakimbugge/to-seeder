aa69de60a89f350d309a9fc87b0275e35a96c9e3
Joakim Bugge <joakim.bugge@gmail.com>
2026-04-05 12:26:47 +0200

    chore: remove internal playground

--- packages/typeorm-seeder/vitest.config.ts
+++ packages/typeorm-seeder/vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       include: ['src/**'],
-      exclude: ['src/index.ts', 'src/playground.ts'],
+      exclude: ['src/index.ts'],
       reporter: ['text', 'lcov'],
     },
   },
